d678703d9fcf34116078193ee38af38fcab17643
tiffanysfw1 <tiffyjemohappy19@gmail.com>
2025-04-16 10:50:30 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['jspdf'], // Exclude jsPDF from bundling
+      // remove external: ['jspdf']
     },
   },
-})
+});
